ac2d1d7e479d3135c3226d2987ada40eedc2f9b4
ghostx31 <himanshu312001@gmail.com>
2023-02-24 09:19:42 +0530

    chore: make BaseInstall.sh more flexible

--- BaseInstall.sh
+++ BaseInstall.sh
@@ -6,50 +6,61 @@ arch-chroot /mnt
 
 ##############################################################3333
 
-
-cd /etc
-echo "Now in /etc directory"
-ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime 
+echo "Enter your region in the form of 'Continent/City', e.g., 'Asia/Kolkata'"
+read loc 
+ln -sf /usr/share/zoneinfo/$loc /etc/localtime 
+echo "Timezone set to $loc"
 hwclock --systohc
 echo "Hardware clock synchronised Successfully"
 
 
 ################################################################
-echo "ArchLinux" >> hostname
+echo "Enter hostname you want to set."
+read hostname
+echo "$hostname" > /etc/hostname
 echo "Hostname assigned Successfully"
 
 
 ###################################################
 rm -rf /etc/hosts
-echo "127.0.0.1     localhost" >> /etc/hosts
-echo "::1     localhost" >> /etc/hosts
-echo "127.0.1.1     ArchLinux.localdomain	ArchLinux" >> /etc/hosts
-
-echo "The following data was written in /etc/hosts"
-echo "127.0.0.1     localhost" >> /etc/hosts
-echo "::1     localhost" >> /etc/hosts
-echo "127.0.1.1     ArchLinux.localdomain	ArchLinux" >> /etc/hosts
+echo -e "127.0.0.1  localhost\n::1    localhost\n127.0.1.1 $hostname.localdomain $hostname" > /etc/hosts
+echo "The following data was written to /etc/hosts"
+echo -e "127.0.0.1  localhost\n::1    localhost\n127.0.1.1 $hostname.localdomain $hostname"
 
 
 ###################################################3
-rm -rf /etc/locale.gen
-echo "en_US.UTF-8 UTF-8" >> locale.gen
-echo "LANG=en_US.UTF-8" >> locale.conf
+echo "Setting locale to en_US.UTF-8"
+sed -i "/#en_US.UTF-8/s/^#//g" /etc/locale.gen
+locale-gen
+echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 echo "Locales configured Successfully"
 
 ####################################################
-rm -rf /etc/resolv.conf
-echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo -e "nameserver 1.1.1.1\nameserver 8.8.8.8\nnameserver8.8.4.4" > /etc/resolv.conf
 chattr +i /etc/resolv.conf
-echo "DNS Resolved , following data was written "
-echo "nameserver 1.1.1.1" >> /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+echo "Namservers configured!"
 ##################################################
 
 sudo pacman -S grub networkmanager network-manager-applet dialog wireless_tools wpa_supplicants base-devel linux-headers firefox xorg-server xorg-xinit libx11 libxinerama libxft webkit2gtk plasma-desktop dolphin konsole  --noconfirm
 
+# You can actually configure this to install grub yourself by taking user's input during disk setup, I'll leave that to you to learn :)
 echo "Install Grub Yourself"
-echo "Set Root Password , Create a user and add it to visudo"
+echo "Enter the password you want to set. The typed password will not appear in the console."
+read -s passw
+echo "Setting root password!"
+echo "root:$passw" | chpasswd
+echo "Checking the new password..."
+su -c "echo 'Password verified Successfully'" - root
+
+echo "Enter the username of the user you want to create."
+read user
+useradd -mG wheel,audio,video,storage,power $user
+
+echo "Set a password for the user. The entered password will not appear in the console."
+read -s password
+echo "$user:$password" | chpasswd
+
+echo "User $user was created successfully."
+
+echo "Enabling root privileges for users in wheel group..."
+sed -i "/%wheel ALL=(ALL:ALL) ALL/s/^#//g" /etc/sudoers
